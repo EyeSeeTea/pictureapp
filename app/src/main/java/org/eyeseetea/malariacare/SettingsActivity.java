a25b64d1ede87047a4b37df9ab5d695e1f114308
idelcano <ignaciodelcano@gmail.com>
2015-10-08 21:32:54 +0200

    Added the remove_all_sent_surveys option at Settings. Added dialog to ask if really want remove all sent surveys.

--- app/src/main/java/org/eyeseetea/malariacare/SettingsActivity.java
+++ app/src/main/java/org/eyeseetea/malariacare/SettingsActivity.java
@@ -20,7 +20,9 @@
 package org.eyeseetea.malariacare;
 
 import android.annotation.TargetApi;
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.res.Configuration;
@@ -31,6 +33,9 @@ import android.preference.Preference;
 import android.preference.PreferenceActivity;
 import android.preference.PreferenceFragment;
 import android.preference.PreferenceManager;
+import android.util.Log;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import org.eyeseetea.malariacare.database.utils.PreferencesState;
 
@@ -90,7 +95,45 @@ public class SettingsActivity extends PreferenceActivity implements SharedPrefer
         bindPreferenceSummaryToValue(findPreference(getApplicationContext().getString(R.string.font_sizes)));
         bindPreferenceSummaryToValue(findPreference(getApplicationContext().getString(R.string.dhis_url)));
         bindPreferenceSummaryToValue(findPreference(getApplicationContext().getString(R.string.org_unit)));
+
+        // Set the ClickListener to the android:key"remove_sent_surveys" preference.
+
+        Preference button = (Preference)findPreference("remove_sent_surveys");
+        button.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+            @Override
+            public boolean onPreferenceClick(Preference preference) {
+                askRemoveSentSurveys();
+                return true;
+            }
+        });
     }
+
+    //ask if the Sent Surveys
+    private void askRemoveSentSurveys() {
+        DialogInterface.OnClickListener dialogClickListener = new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                switch (which){
+                    case DialogInterface.BUTTON_POSITIVE:
+                        removeSentSurveys();
+                        break;
+
+                    case DialogInterface.BUTTON_NEGATIVE:
+                        break;
+                }
+            }
+        };
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this.getListView().getContext());
+        builder.setMessage(R.string.dialog_title_delete_surveys).setPositiveButton(R.string.yes, dialogClickListener)
+                .setNegativeButton(R.string.no, dialogClickListener).show();
+    }
+
+    private void removeSentSurveys() {
+
+
+    }
+
 
     /**
      * {@inheritDoc}
@@ -174,7 +217,7 @@ public class SettingsActivity extends PreferenceActivity implements SharedPrefer
      */
     private static void bindPreferenceSummaryToValue(Preference preference) {
         // Set the listener to watch for value changes.
-         preference.setOnPreferenceChangeListener(sBindPreferenceSummaryToValueListener);
+        preference.setOnPreferenceChangeListener(sBindPreferenceSummaryToValueListener);
 
         // Trigger the listener immediately with the preference's
         // current value.

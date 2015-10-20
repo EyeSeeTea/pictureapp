e1dd1906d7509b9bcc9e53c0d15e33b5858fee62
idelcano <ignaciodelcano@gmail.com>
2015-10-20 02:46:49 +0200

    Added funcionality for check if the select org_unit is valid

--- app/src/main/java/org/eyeseetea/malariacare/views/AutoCompleteEditTextPreference.java
+++ app/src/main/java/org/eyeseetea/malariacare/views/AutoCompleteEditTextPreference.java
@@ -3,38 +3,20 @@ package org.eyeseetea.malariacare.views;
 
 import android.app.Activity;
 import android.content.Context;
-import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.AsyncTask;
 import android.preference.EditTextPreference;
-import android.preference.PreferenceManager;
 import android.util.AttributeSet;
-import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.ViewParent;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
-import android.widget.EditText;
 
-import com.squareup.okhttp.Authenticator;
-import com.squareup.okhttp.Credentials;
-import com.squareup.okhttp.OkHttpClient;
-import com.squareup.okhttp.Request;
-import com.squareup.okhttp.RequestBody;
 import com.squareup.okhttp.Response;
 
 import org.eyeseetea.malariacare.R;
-import org.eyeseetea.malariacare.database.utils.PreferencesState;
 import org.eyeseetea.malariacare.network.PushClient;
-import org.eyeseetea.malariacare.network.PushResult;
-import org.eyeseetea.malariacare.network.UnsafeOkHttpsClientFactory;
-import org.eyeseetea.malariacare.services.SurveyService;
-import org.json.JSONArray;
-import org.json.JSONObject;
 
-import java.io.IOException;
-import java.net.Proxy;
 import java.util.ArrayList;
 import java.util.concurrent.ExecutionException;
 
@@ -57,7 +39,7 @@ public class AutoCompleteEditTextPreference extends EditTextPreference {
             ArrayList<String> opcionesGet = new ArrayList<String>();
             String[]  orgUnits= null;
             try {
-                orgUnits = new getBackground().execute(opcionesGet).get();
+                orgUnits = new GetOrgUnitsAsync().execute(opcionesGet).get();
             } catch (InterruptedException e) {
                 e.printStackTrace();
             } catch (ExecutionException e) {
@@ -71,6 +53,7 @@ public class AutoCompleteEditTextPreference extends EditTextPreference {
             ArrayAdapter<String> adapter = new ArrayAdapter(this.getContext(),
                     android.R.layout.simple_dropdown_item_1line,orgUnits);
             mEditText.setAdapter(adapter);
+
         }
     }
 
@@ -94,11 +77,29 @@ public class AutoCompleteEditTextPreference extends EditTextPreference {
             String value = mEditText.getText().toString();
             if (callChangeListener(value)) {
                 setText(value);
+                try {
+                   boolean orgUnits = new CheckCodeAsync().execute(value).get();
+                    if(!orgUnits)
+                    {
+                        try {
+                            throw new ShowException(this.getContext().getString(R.string.exception_org_unit_not_valid), this.getContext());
+                        } catch (ShowException e) {
+                            e.printStackTrace();
+                        }
+                        //If the orgUnit change, maybe is it unbanned..
+                        PushClient.setUnbanned();
+                    }
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } catch (ExecutionException e) {
+                    e.printStackTrace();
+                }
+
             }
         }
     }
 
-    class getBackground extends AsyncTask<ArrayList<String>, Void, String[]> {
+    class GetOrgUnitsAsync extends AsyncTask<ArrayList<String>, Void, String[]> {
 
         @Override
         protected void onPreExecute() {
@@ -120,4 +121,32 @@ public class AutoCompleteEditTextPreference extends EditTextPreference {
         }
 
     }
+
+    class CheckCodeAsync extends AsyncTask<String, Void, Boolean> {
+
+        @Override
+        protected void onPreExecute() {
+        }
+
+        protected Boolean doInBackground(String... param) {
+            boolean result = false;
+
+            String orgUnit = param[0];
+            try {
+                PushClient pushClient=new PushClient((Activity)getContext());
+                result = pushClient.checkOrgUnit(orgUnit);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            if(result)
+                return Boolean.TRUE;
+                else
+            return Boolean.FALSE;
+        }
+
+        protected void onPostExecute(Response result) {
+        }
+
+    }
+
 }

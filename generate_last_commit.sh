9d70ffcdb65f014e125eb837cc6826f1c9fdc87d
Adrian Quintana <adrian.perez@ucl.ac.uk>
2016-04-25 22:08:25 +0100

    added about us and docs and last commit

---
+++ generate_last_commit.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+gitPath=$(git rev-parse --show-toplevel)
+sh ${gitPath}/generate_last_commit.sh
+#!/bin/sh
+gitPath=$(git rev-parse --show-toplevel)
+filePath="${gitPath}/app/src/main/res/raw/lastcommit.txt"
+echo "Last commit path $filePath"
+commit=$(git log -1 HEAD --format=%H)
+echo "Saving last commit: ${commit}"
+echo ${commit} > $filePath
+echo "Done."

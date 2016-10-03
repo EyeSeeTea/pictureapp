ff79f9c490427722e12bcce230cf7f09b3410994
idelcano <ignaciodelcano@gmail.com>
2016-10-03 20:50:32 +0200

    fix migrationfix migrationss

--- app/src/main/java/org/eyeseetea/malariacare/database/migrations/Migration13ModifyValuesLastMigration.java
+++ app/src/main/java/org/eyeseetea/malariacare/database/migrations/Migration13ModifyValuesLastMigration.java
@@ -14,21 +14,20 @@ import org.eyeseetea.malariacare.database.utils.PreferencesState;
 import java.io.IOException;
 
 /**
- * Created by idelcano on 23/09/2016.
+ * Created by idelcano on 03/08/2016.
  */
+@Migration(version = 13, databaseName = AppDatabase.NAME)
+public class Migration13ModifyValuesLastMigration extends BaseMigration {
 
-@Migration(version = 10, databaseName = AppDatabase.NAME)
-public class Migration10UpdateOptionAttributes extends BaseMigration {
+    private static String TAG=".Migration13";
 
-    private static String TAG = ".Migration10";
-
-    private static Migration10UpdateOptionAttributes instance;
+    private static Migration13ModifyValuesLastMigration instance;
     private boolean postMigrationRequired;
 
-    public Migration10UpdateOptionAttributes() {
+    public Migration13ModifyValuesLastMigration() {
         super();
         instance = this;
-        postMigrationRequired = false;
+        postMigrationRequired=false;
     }
 
     public void onPreMigrate() {
@@ -36,7 +35,7 @@ public class Migration10UpdateOptionAttributes extends BaseMigration {
 
     @Override
     public void migrate(SQLiteDatabase database) {
-        postMigrationRequired = true;
+        postMigrationRequired=true;
     }
 
     @Override
@@ -44,34 +43,34 @@ public class Migration10UpdateOptionAttributes extends BaseMigration {
     }
 
 
-    public static void postMigrate() {
+    public static void postMigrate(){
         //Migration NOT required -> done
-        Log.d(TAG, "Post migrate");
-        if (!instance.postMigrationRequired) {
+        Log.d(TAG,"Post migrate");
+        if(!instance.postMigrationRequired){
             return;
         }
 
 
         //Data? Add new default data
-
-        if (instance.hasData()) {
+        if(instance.hasData()) {
             try {
                 PopulateDB.addOptionAttributes(PreferencesState.getInstance().getContext().getAssets());
+                PopulateDB.updateOptionNames(PreferencesState.getInstance().getContext().getAssets());
+                PopulateDB.updateQuestions(PreferencesState.getInstance().getContext().getAssets());
             } catch (IOException e) {
                 e.printStackTrace();
             }
         }
 
         //This operation wont be done again
-        instance.postMigrationRequired = false;
+        instance.postMigrationRequired=false;
     }
 
     /**
      * Checks if the current db has data or not
-     *
      * @return
      */
     private boolean hasData() {
-        return Program.getFirstProgram() != null;
+        return Program.getFirstProgram()!=null;
     }
 }

8ec50a7984a1bf06d9856081db3ba61bc3b95d69
idelcano <ignaciodelcano@gmail.com>
2016-12-15 14:36:11 +0100

    Revert "Fix readOnly totalQuestions calc"
    This reverts commit bbcb3509be5090377658b8018686c73028a5e6ba.

--- app/src/main/java/org/eyeseetea/malariacare/layout/adapters/survey/DynamicTabAdapter.java
+++ app/src/main/java/org/eyeseetea/malariacare/layout/adapters/survey/DynamicTabAdapter.java
@@ -170,7 +170,10 @@ public class DynamicTabAdapter extends BaseAdapter implements ITabAdapter {
         int totalPages = navigationController.getCurrentQuestion().getTotalQuestions();
         if (readOnly) {
             if (Session.getSurvey() != null) {
-                totalPages = Session.getSurvey().getMaxTotalPages();
+                Question lastQuestion = Session.getSurvey().findLastSavedQuestion();
+                if (lastQuestion != null) {
+                    totalPages = lastQuestion.getTotalQuestions();
+                }
             }
         }
         navigationController.setTotalPages(totalPages);
@@ -1509,6 +1512,7 @@ public class DynamicTabAdapter extends BaseAdapter implements ITabAdapter {
 
         question = navigationController.getCurrentQuestion();
         value = question.getValueBySession();
+        //TODO: rhardjono: navigationController totalPages is not updating properly in survey review
         //set new page number if the value is null
         if (value == null && !readOnly) {
             navigationController.setTotalPages(

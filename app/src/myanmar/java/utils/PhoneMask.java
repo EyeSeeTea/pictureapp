0856fb1a34f1bb982e8ee8c7c0e9090f2e4e0900
ifoche <nacho.foche@gmail.com>
2016-12-15 11:20:43 +0100

    Mask fixed to 7-15 digits and comments deleted

--- app/src/myanmar/java/utils/PhoneMask.java
+++ app/src/myanmar/java/utils/PhoneMask.java
@@ -9,11 +9,8 @@ public class PhoneMask {
      * Plain telephone mask: between xxxxxxx = 7 and xxxxxxxxxxxxxxx = 15
      */
     public static final String PLAIN_PHONE_NUMBER_MASK =
-            "^\\d{5,15}$";
+            "^\\d{7,15}$";
 
-    /**
-     * Formats number according to mask (xxx) xxx-xxx = 9 (020) xxxx-xxxx = 11  (030) xxx-xxxx = 10
-     */
     public static String formatPhoneNumber(String phoneValue) {
         //Empty -> nothing to format
         if (phoneValue == null) {
@@ -27,13 +24,7 @@ public class PhoneMask {
 
         return phoneValue;
     }
-
-    /**
-     * Checks if the given string corresponds a correct phone number according to mask:
-     * (xxx) xxx-xxx = 9 (020) xxxx-xxxx = 11  (030) xxx-xxxx = 10
-     *
-     * @return true|false
-     */
+    
     public static boolean checkPhoneNumberByMask(String phoneValue) {
 
         //Empty  is ok

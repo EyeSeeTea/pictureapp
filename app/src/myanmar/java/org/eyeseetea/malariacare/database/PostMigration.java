73a4c722ff7ecec77435a7ea8a4a406cf19edb58
mplazaspalacio <manuelplazaspalacio@gmail.com>
2017-01-04 19:12:33 +0100

    Add new migration file, change database version and calling migration22 from postMigration class

--- app/src/myanmar/java/org/eyeseetea/malariacare/database/PostMigration.java
+++ app/src/myanmar/java/org/eyeseetea/malariacare/database/PostMigration.java
@@ -1,6 +1,7 @@
 package org.eyeseetea.malariacare.database;
 
 import org.eyeseetea.malariacare.database.migrations.Migration15AddOptionAttributeColumns;
+import org.eyeseetea.malariacare.database.migrations.Migration22AddStockCsvs;
 
 /**
  * Created by idelcano on 29/09/2016.
@@ -10,5 +11,6 @@ public class PostMigration {
 
     public static void launchPostMigration() {
         Migration15AddOptionAttributeColumns.postMigrate();
+        Migration22AddStockCsvs.postMigrate();
     }
 }

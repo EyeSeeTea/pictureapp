646d8cfe5369609da6fd17f3a49521c120eaed8a
idelcano <ignaciodelcano@gmail.com>
2017-01-09 12:29:54 +0100

    Restore title on review screen

--- app/src/myanmar/java/org/eyeseetea/malariacare/strategies/ReviewFragmentStrategy.java
+++ app/src/myanmar/java/org/eyeseetea/malariacare/strategies/ReviewFragmentStrategy.java
@@ -12,18 +12,22 @@ import org.eyeseetea.sdk.presentation.views.CustomTextView;
 
 public class ReviewFragmentStrategy extends AReviewFragmentStrategy {
 
+    final String TITLE_SEPARATOR=": ";
+
     public TableRow createViewRow(TableRow rowView, Value value) {
         //Sets the value text in the row and add the question as tag.
-        CustomTextView customTextView = (CustomTextView) rowView.findViewById(
+        CustomTextView valueTextView = (CustomTextView) rowView.findViewById(
                 R.id.review_content_text);
-        customTextView.setText(
+        CustomTextView questionTextView = (CustomTextView) rowView.findViewById(
+                R.id.review_title_text);
+        valueTextView.setText(
                 (value.getOption() != null) ? value.getOption().getInternationalizedCode()
                         : value.getValue());
         if ((value.getQuestion() != null)) {
-            customTextView.setTag(value.getQuestion());
-
+            valueTextView.setTag(value.getQuestion());
+            questionTextView.setText(value.getQuestion().getInternationalizedCodeDe_Name()+TITLE_SEPARATOR);
             //Adds click listener to hide the fragment and go to the clicked question.
-            customTextView.setOnClickListener(new View.OnClickListener() {
+            valueTextView.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
                     Question question = (Question) v.getTag();
@@ -32,7 +36,7 @@ public class ReviewFragmentStrategy extends AReviewFragmentStrategy {
             });
 
             if (value.getOption() != null && value.getOption().getBackground_colour() != null) {
-                customTextView.setBackgroundColor(
+                valueTextView.setBackgroundColor(
                         Color.parseColor("#" + value.getOption().getBackground_colour()));
             }
 

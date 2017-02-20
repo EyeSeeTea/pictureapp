dda83cd3c2133389dfb12112b04b8282f1907a42
mplazaspalacio <manuelplazaspalacio@gmail.com>
2017-02-20 13:34:37 +0100

    Change main treatment with not main treatment relations

--- app/src/main/java/org/eyeseetea/malariacare/data/database/model/Treatment.java
+++ app/src/main/java/org/eyeseetea/malariacare/data/database/model/Treatment.java
@@ -4,6 +4,8 @@ import static org.eyeseetea.malariacare.data.database.AppDatabase.drugAlias;
 import static org.eyeseetea.malariacare.data.database.AppDatabase.drugCombinationAlias;
 import static org.eyeseetea.malariacare.data.database.AppDatabase.drugCombinationName;
 import static org.eyeseetea.malariacare.data.database.AppDatabase.drugName;
+import static org.eyeseetea.malariacare.data.database.AppDatabase.matchAlias;
+import static org.eyeseetea.malariacare.data.database.AppDatabase.matchName;
 
 import com.raizlabs.android.dbflow.annotation.Column;
 import com.raizlabs.android.dbflow.annotation.PrimaryKey;
@@ -81,30 +83,23 @@ public class Treatment extends BaseModel {
     }
 
     public List<Treatment> getAlternativeTreatments() {
+        Match match=new Select()
+                .from(Match.class).as(matchName)
+                .join(TreatmentMatch.class, Join.JoinType.INNER).as(treatmentMatchName)
+                .on(Match_Table.id_match.withTable(matchAlias).is(TreatmentMatch_Table.id_match.withTable(treatmentMatchAlias)))
+                .where(TreatmentMatch_Table.id_treatment.withTable(treatmentMatchAlias).is(id_treatment))
+                .querySingle();
         List<Treatment> treatments = new Select()
                 .from(Treatment.class).as(treatmentName)
-                .join(TreatmentMatch.class, Join.JoinType.LEFT_OUTER).as(treatmentMatchName)
+                .join(TreatmentMatch.class, Join.JoinType.INNER).as(treatmentMatchName)
                 .on(Treatment_Table.id_treatment.withTable(treatmentAlias)
-                        .eq(TreatmentMatch_Table.id_match.withTable(treatmentMatchAlias)))
-                .where(TreatmentMatch_Table.id_treatment.withTable(treatmentMatchAlias)
-                        .is(id_treatment))
+                        .eq(TreatmentMatch_Table.id_treatment.withTable(treatmentMatchAlias)))
+                .where(TreatmentMatch_Table.id_match.withTable(treatmentMatchAlias)
+                        .is(match.getId_match()))
                 .and(Treatment_Table.type.withTable(treatmentAlias).is(TYPE_NOT_MAIN))
                 .queryList();
-
-        //TODO remove this related with DBFlow bug overriding ids
-        List<Treatment> correctIdTreatments = new ArrayList<>();
-        for (Treatment treatment : treatments) {
-            Treatment correctTreatment = new Select()
-                    .from(Treatment.class)
-                    .where(Treatment_Table.diagnosis.is(treatment.getDiagnosis()))
-                    .and(Treatment_Table.message.is(treatment.getMessage()))
-                    .and(Treatment_Table.id_organisation.is(
-                            treatment.getOrganisation().getId_organisation()))
-                    .and(Treatment_Table.type.is(treatment.getType()))
-                    .querySingle();
-            correctIdTreatments.add(correctTreatment);
-        }
-        return correctIdTreatments;
+        //FIXME: select split in two because DBFLOW bug
+        return treatments;
     }
 
     public long getId_treatment() {

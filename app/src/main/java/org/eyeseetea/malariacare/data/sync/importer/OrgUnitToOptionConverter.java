2b5300b2c4426eae4ec1b038d24e3d911dd4cb4e
Jorge Sánchez <xurxosf@gmail.com>
2017-02-22 10:06:43 +0100

    separate org unit as question option creation and cleaning. Creation is realized in pull process and cleaning is realized in logout.

--- app/src/main/java/org/eyeseetea/malariacare/data/sync/importer/OrgUnitToOptionConverter.java
+++ app/src/main/java/org/eyeseetea/malariacare/data/sync/importer/OrgUnitToOptionConverter.java
@@ -3,24 +3,17 @@ package org.eyeseetea.malariacare.data.sync.importer;
 import org.eyeseetea.malariacare.data.database.model.Option;
 import org.eyeseetea.malariacare.data.database.model.OrgUnit;
 import org.eyeseetea.malariacare.data.database.model.Question;
-import org.eyeseetea.malariacare.data.database.model.QuestionOption;
 
 import java.util.List;
 
 public class OrgUnitToOptionConverter {
     public static void convert() {
         List<Question> questions = Question.getAllQuestionsWithOrgUnitDropdownList();
-        //remove older values, but not the especial "other" option
-        for (Question question : questions) {
-            List<Option> options = question.getAnswer().getOptions();
-            removeOldValues(question, options);
-        }
 
         if (questions.size() == 0) {
             return;
         }
 
-        //Generate the orgUnits options for each question with orgunit dropdown list
         List<OrgUnit> orgUnits = OrgUnit.getAllOrgUnit();
         for (OrgUnit orgUnit : orgUnits) {
             addOUOptionToQuestions(questions, orgUnit);
@@ -50,13 +43,5 @@ public class OrgUnitToOptionConverter {
         }
 
         return false;
-    }
-
-    public static void removeOldValues(Question question, List<Option> options) {
-        for (Option option : options) {
-            if (QuestionOption.findByQuestionAndOption(question, option).size() == 0) {
-                option.delete();
-            }
-        }
     }
 }

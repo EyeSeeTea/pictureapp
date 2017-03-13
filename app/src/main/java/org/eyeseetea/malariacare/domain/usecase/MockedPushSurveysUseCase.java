b6c0d1363de6dea40f72ae9046eeeb9f5aaff810
idelcano <ignaciodelcano@gmail.com>
2017-03-13 17:09:32 +0100

    Fix push use case

--- app/src/main/java/org/eyeseetea/malariacare/domain/usecase/MockedPushSurveysUseCase.java
+++ app/src/main/java/org/eyeseetea/malariacare/domain/usecase/MockedPushSurveysUseCase.java
@@ -9,6 +9,19 @@ import java.util.List;
 
 public class MockedPushSurveysUseCase {
     public void execute(Callback callback) {
+        List<Survey> surveys = Survey.getAllMalariaSurveysToBeSent();
+
+        //Check surveys not in progress
+        for (Survey survey : surveys) {
+            survey.setStatus(SURVEY_SENT);
+            survey.save();
+            Survey stockSurvey = Survey.getStockSurveyWithEventDate(survey.getEventDate());
+            if (stockSurvey != null) {
+                stockSurvey.setStatus(SURVEY_SENT);
+                stockSurvey.save();
+            }
+        }
+        callback.onPushFinished();
     }
 
     public interface Callback {

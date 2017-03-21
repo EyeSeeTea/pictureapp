a45e83928de8f794e00bae7d60551d7723e5c26d
idelcano <ignaciodelcano@gmail.com>
2017-03-21 09:23:43 +0100

    Remove import

--- app/src/main/java/org/eyeseetea/malariacare/fragments/SurveyFragment.java
+++ app/src/main/java/org/eyeseetea/malariacare/fragments/SurveyFragment.java
@@ -56,7 +56,6 @@ import org.eyeseetea.sdk.presentation.views.CustomTextView;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Iterator;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;

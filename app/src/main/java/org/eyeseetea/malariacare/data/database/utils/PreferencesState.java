66e24f9d1c729e49e5427c583f502dc2e497101f
Jorge Sánchez <xurxosf@gmail.com>
2017-03-21 09:49:23 +0100

    load font scales in preferences from sdk font styles

--- app/src/main/java/org/eyeseetea/malariacare/data/database/utils/PreferencesState.java
+++ app/src/main/java/org/eyeseetea/malariacare/data/database/utils/PreferencesState.java
@@ -19,6 +19,8 @@
 
 package org.eyeseetea.malariacare.data.database.utils;
 
+import static java.lang.reflect.Array.getInt;
+
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.content.res.Resources;
@@ -28,13 +30,11 @@ import android.util.Log;
 
 import org.eyeseetea.malariacare.R;
 import org.eyeseetea.malariacare.domain.entity.Credentials;
-import org.eyeseetea.malariacare.utils.Constants;
+import org.eyeseetea.sdk.presentation.styles.FontStyle;
 
 import java.util.Calendar;
 import java.util.Date;
-import java.util.HashMap;
 import java.util.Locale;
-import java.util.Map;
 
 /**
  * Singleton that holds info related to preferences
@@ -55,7 +55,7 @@ public class PreferencesState {
     /**
      * Selected scale, one between [xsmall,small,medium,large,xlarge,system]
      */
-    private String scale;
+    private FontStyle fontStyle;
     /**
      * Flag that determines if numerator/denominator are shown in scores.
      */
@@ -72,10 +72,6 @@ public class PreferencesState {
      * Specified DHIS2 Server
      */
     private String dhisURL;
-    /**
-     * Map that holds the relationship between a scale and a set of dimensions
-     */
-    private Map<String, Map<String, Float>> scaleDimensionsMap;
 
     private PreferencesState() {
     }
@@ -106,7 +102,6 @@ public class PreferencesState {
 
     public void init(Context context) {
         this.context = context;
-        scaleDimensionsMap = initScaleDimensionsMap();
         reloadPreferences();
     }
 
@@ -115,7 +110,7 @@ public class PreferencesState {
     }
 
     public void reloadPreferences() {
-        scale = initScale();
+        fontStyle = initFontStyle();
         showNumDen = initShowNumDen();
         orgUnit = initOrgUnit();
         dhisURL = initDhisURL();
@@ -123,7 +118,7 @@ public class PreferencesState {
         Log.d(TAG, "reloadPreferences: "
                 + " orgUnit:" + orgUnit
                 + " |dhisURL:" + dhisURL
-                + " |scale:" + scale
+                + " |fontStyle:" + fontStyle.getTitle()
                 + " | showNumDen:" + showNumDen);
     }
 
@@ -156,19 +151,22 @@ public class PreferencesState {
                 instance.getContext().getString(R.string.DHIS_DEFAULT_SERVER));
     }
 
-    /**
-     * Inits scale according to preferences
-     */
-    private String initScale() {
+    private FontStyle initFontStyle() {
         SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(
                 instance.getContext());
+
         if (sharedPreferences.getBoolean(instance.getContext().getString(R.string.customize_fonts),
                 false)) {
-            return sharedPreferences.getString(instance.getContext().getString(R.string.font_sizes),
-                    Constants.FONTS_SYSTEM);
+            String fontStyleId = sharedPreferences.getString(instance.getContext().getString(R.string.font_sizes),
+                    String.valueOf(FontStyle.Medium.getResId()));
+
+            for (FontStyle fontStyle:FontStyle.values()) {
+                if (fontStyle.getResId() == Integer.valueOf(fontStyleId))
+                    return fontStyle;
+            }
         }
 
-        return Constants.FONTS_SYSTEM;
+        return FontStyle.Medium;
     }
 
     /**
@@ -181,62 +179,12 @@ public class PreferencesState {
                 false);
     }
 
-    /**
-     * Inits maps of dimensions
-     */
-    private Map<String, Map<String, Float>> initScaleDimensionsMap() {
-        Map<String, Float> xsmall = new HashMap<>();
-        String xsmallKey = instance.getContext().getString(R.string.font_size_level0),
-                smallKey = context.getString(R.string.font_size_level1),
-                mediumKey = context.getString(R.string.font_size_level2),
-                largeKey = context.getString(R.string.font_size_level3),
-                xlargeKey = context.getString(R.string.font_size_level4);
-
-        xsmall.put(xsmallKey, context.getResources().getDimension(R.dimen.xsmall_xsmall_text_size));
-        xsmall.put(smallKey, context.getResources().getDimension(R.dimen.xsmall_small_text_size));
-        xsmall.put(mediumKey, context.getResources().getDimension(R.dimen.xsmall_medium_text_size));
-        xsmall.put(largeKey, context.getResources().getDimension(R.dimen.xsmall_large_text_size));
-        xsmall.put(xlargeKey, context.getResources().getDimension(R.dimen.xsmall_xlarge_text_size));
-        Map<String, Float> small = new HashMap<>();
-        small.put(xsmallKey, context.getResources().getDimension(R.dimen.small_xsmall_text_size));
-        small.put(smallKey, context.getResources().getDimension(R.dimen.small_small_text_size));
-        small.put(mediumKey, context.getResources().getDimension(R.dimen.small_medium_text_size));
-        small.put(largeKey, context.getResources().getDimension(R.dimen.small_large_text_size));
-        small.put(xlargeKey, context.getResources().getDimension(R.dimen.small_xlarge_text_size));
-        Map<String, Float> medium = new HashMap<>();
-        medium.put(xsmallKey, context.getResources().getDimension(R.dimen.medium_xsmall_text_size));
-        medium.put(smallKey, context.getResources().getDimension(R.dimen.medium_small_text_size));
-        medium.put(mediumKey, context.getResources().getDimension(R.dimen.medium_medium_text_size));
-        medium.put(largeKey, context.getResources().getDimension(R.dimen.medium_large_text_size));
-        medium.put(xlargeKey, context.getResources().getDimension(R.dimen.medium_xlarge_text_size));
-        Map<String, Float> large = new HashMap<>();
-        large.put(xsmallKey, context.getResources().getDimension(R.dimen.large_xsmall_text_size));
-        large.put(smallKey, context.getResources().getDimension(R.dimen.large_small_text_size));
-        large.put(mediumKey, context.getResources().getDimension(R.dimen.large_medium_text_size));
-        large.put(largeKey, context.getResources().getDimension(R.dimen.large_large_text_size));
-        large.put(xlargeKey, context.getResources().getDimension(R.dimen.large_xlarge_text_size));
-        Map<String, Float> xlarge = new HashMap<>();
-        xlarge.put(xsmallKey, context.getResources().getDimension(R.dimen.extra_xsmall_text_size));
-        xlarge.put(smallKey, context.getResources().getDimension(R.dimen.extra_small_text_size));
-        xlarge.put(mediumKey, context.getResources().getDimension(R.dimen.extra_medium_text_size));
-        xlarge.put(largeKey, context.getResources().getDimension(R.dimen.extra_large_text_size));
-        xlarge.put(xlargeKey, context.getResources().getDimension(R.dimen.extra_xlarge_text_size));
-
-        Map scaleDimensionsMap = new HashMap<>();
-        scaleDimensionsMap.put(xsmallKey, xsmall);
-        scaleDimensionsMap.put(smallKey, small);
-        scaleDimensionsMap.put(mediumKey, medium);
-        scaleDimensionsMap.put(largeKey, large);
-        scaleDimensionsMap.put(xlargeKey, xlarge);
-        return scaleDimensionsMap;
+    public FontStyle getFontStyle() {
+        return fontStyle;
     }
 
-    public String getScale() {
-        return scale;
-    }
-
-    public void setScale(String value) {
-        this.scale = value;
+    public void setFontStyle(FontStyle fontStyle) {
+        this.fontStyle = fontStyle;
     }
 
     public boolean isShowNumDen() {
@@ -253,10 +201,6 @@ public class PreferencesState {
 
     public void setIsNewServerUrl(boolean value) {
         this.isNewServerUrl = value;
-    }
-
-    public Float getFontSize(String scale, String dimension) {
-        return scaleDimensionsMap.get(scale).get(dimension);
     }
 
     public String getOrgUnit() {

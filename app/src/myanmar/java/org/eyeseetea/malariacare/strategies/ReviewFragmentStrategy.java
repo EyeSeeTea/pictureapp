d65326cd8b7f70b24208cb8679c32efc9e1fbf7a
idelcano <ignaciodelcano@gmail.com>
2017-03-27 13:00:01 +0200

    Refactor isStockSurvey to myanmar variant

--- app/src/myanmar/java/org/eyeseetea/malariacare/strategies/ReviewFragmentStrategy.java
+++ app/src/myanmar/java/org/eyeseetea/malariacare/strategies/ReviewFragmentStrategy.java
@@ -9,6 +9,7 @@ import org.eyeseetea.malariacare.R;
 import org.eyeseetea.malariacare.data.database.model.Question;
 import org.eyeseetea.malariacare.data.database.model.Value;
 import org.eyeseetea.malariacare.data.database.utils.PreferencesState;
+import org.eyeseetea.malariacare.data.database.utils.Session;
 import org.eyeseetea.malariacare.layout.adapters.survey.DynamicTabAdapter;
 import org.eyeseetea.sdk.presentation.views.CustomTextView;
 
@@ -72,4 +73,20 @@ public class ReviewFragmentStrategy extends AReviewFragmentStrategy {
         }
         return question;
     }
+
+
+    public static boolean isValidValue(Value value) {
+        if (Session.getStockSurvey()==null || value.getQuestion() == null) {
+            return false;
+        }
+        for (Value stockValue : Session.getStockSurvey().getValuesFromDB()) {
+            if (stockValue.getQuestion() != null) {
+                if (stockValue.getQuestion().getUid().equals(value.getQuestion().getUid())) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
 }

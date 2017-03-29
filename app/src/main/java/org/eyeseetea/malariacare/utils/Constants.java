974f69e724c3a80f3585b9950ff960e360cacac4
mplazaspalacio <manuelplazaspalacio@gmail.com>
2017-03-29 14:23:21 +0200

    Add year picker dialog

--- app/src/main/java/org/eyeseetea/malariacare/utils/Constants.java
+++ app/src/main/java/org/eyeseetea/malariacare/utils/Constants.java
@@ -34,7 +34,8 @@ public class Constants {
             IMAGE_RADIO_GROUP = 25,
             POSITIVE_OR_ZERO_INT = 26,
             DYNAMIC_TREATMENT_SWITCH_NUMBER = 27,
-            DYNAMIC_STOCK_IMAGE_RADIO_BUTTON = 28;
+            DYNAMIC_STOCK_IMAGE_RADIO_BUTTON = 28,
+            YEAR = 29;
 
 
     public static final List<Integer> QUESTION_TYPES_WITH_OPTIONS = Arrays.asList(
---
+++ app/src/main/java/org/eyeseetea/malariacare/views/YearPicker.java
@@ -0,0 +1,72 @@
+package org.eyeseetea.malariacare.views;
+
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.content.Context;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.NumberPicker;
+
+import org.eyeseetea.malariacare.R;
+import org.joda.time.DateTime;
+
+
+public class YearPicker extends DialogFragment {
+    private int YEAR_INTERVAL = 200;
+
+    private OnYearSelectedListener mOnYearSelectedListener;
+    private NumberPicker mNumberPicker;
+    private Button ok, cancel;
+    private int maxYear, minYear;
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        Context context = getActivity();
+        Dialog dialog = new Dialog(context);
+        dialog.setContentView(R.layout.fragment_year_picker);
+        setMaxMinYears();
+        initViews(dialog);
+        return dialog;
+    }
+
+    private void initViews(final Dialog dialog) {
+        mNumberPicker = (NumberPicker) dialog.findViewById(R.id.year_picker);
+        mNumberPicker.setMaxValue(maxYear);
+        mNumberPicker.setMinValue(minYear);
+        mNumberPicker.setValue(maxYear);
+        ok = (Button) dialog.findViewById(R.id.ok);
+        ok.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (mOnYearSelectedListener != null) {
+                    mOnYearSelectedListener.onYearSelected(mNumberPicker.getValue());
+                    dialog.cancel();
+                }
+            }
+        });
+        cancel = (Button) dialog.findViewById(R.id.cancel);
+        cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dialog.cancel();
+            }
+        });
+    }
+
+    private void setMaxMinYears() {
+        DateTime dateTime = new DateTime();
+        maxYear = dateTime.getYear();
+        minYear = dateTime.getYear() - YEAR_INTERVAL;
+    }
+
+
+    public void setOnYearSelectedListener(
+            OnYearSelectedListener onYearSelectedListener) {
+        mOnYearSelectedListener = onYearSelectedListener;
+    }
+
+    public interface OnYearSelectedListener {
+        void onYearSelected(int year);
+    }
+}

f3cefcc650852af092163d4298eeca6f8c22aefa
Jorge Sánchez <xurxosf@gmail.com>
2017-04-05 07:43:00 +0200

    refactor migration from 5 to 7 and fix query bugs

--- app/src/main/java/org/eyeseetea/malariacare/data/database/migrations/Migration7NameByCode.java
+++ app/src/main/java/org/eyeseetea/malariacare/data/database/migrations/Migration7NameByCode.java
@@ -1,10 +1,13 @@
 package org.eyeseetea.malariacare.data.database.migrations;
 
+import com.raizlabs.android.dbflow.annotation.Migration;
 import com.raizlabs.android.dbflow.sql.migration.BaseMigration;
 import com.raizlabs.android.dbflow.structure.database.DatabaseWrapper;
 
+import org.eyeseetea.malariacare.data.database.AppDatabase;
 
-public class Migration5NameByCode extends BaseMigration {
+@Migration(version = 7, database = AppDatabase.class)
+public class Migration7NameByCode extends BaseMigration {
     private static String TAG = ".Migration5";
 
     @Override
@@ -16,9 +19,9 @@ public class Migration5NameByCode extends BaseMigration {
 
         String sqlCopyOption =
                 "INSERT INTO OptionTemp(id_option, code, name, factor, id_answer_fk, "
-                        + "id_option_attribute_fk)"
+                        + "id_option_attribute_fk) "
                         + "SELECT id_option, name, code, factor, id_answer_fk, "
-                        + "id_option_attribute_fk"
+                        + "id_option_attribute_fk "
                         + "FROM Option;";
         database.execSQL(sqlCopyOption);
 

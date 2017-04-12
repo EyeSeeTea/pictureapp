668f5edaf0516d672b436c482d46c9483de6e324
mplazaspalacio <manuelplazaspalacio@gmail.com>
2017-04-12 19:47:42 +0200

    Solve csv bugs an select the correct program for the survey

--- app/src/ereferrals/java/org/eyeseetea/malariacare/strategies/DashboardActivityStrategy.java
+++ app/src/ereferrals/java/org/eyeseetea/malariacare/strategies/DashboardActivityStrategy.java
@@ -2,12 +2,11 @@ package org.eyeseetea.malariacare.strategies;
 
 import android.app.Activity;
 
-import com.raizlabs.android.dbflow.sql.language.Select;
-
 import org.eyeseetea.malariacare.DashboardActivity;
 import org.eyeseetea.malariacare.LoginActivity;
 import org.eyeseetea.malariacare.data.database.model.Program;
 import org.eyeseetea.malariacare.data.database.model.Survey;
+import org.eyeseetea.malariacare.data.database.utils.PreferencesEReferral;
 import org.eyeseetea.malariacare.data.database.utils.Session;
 
 /**
@@ -29,7 +28,7 @@ public class DashboardActivityStrategy extends ADashboardActivityStrategy {
 
     @Override
     public void newSurvey(Activity activity) {
-        Program program = new Select().from(Program.class).querySingle();
+        Program program = Program.findById(PreferencesEReferral.getUserProgramId());
         // Put new survey in session
         Survey survey = new Survey(null, program, Session.getUser());
         survey.save();

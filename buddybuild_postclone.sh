278e09a551a330ae3ab6ff990816b0d98a4ab407
ifoche <nacho.foche@gmail.com>
2017-05-22 15:46:56 +0200

    fix buddybuild automatically get flavor from environment var

--- buddybuild_postclone.sh
+++ buddybuild_postclone.sh
@@ -3,7 +3,7 @@
 # Definitions
 gitPath=$(git rev-parse --show-toplevel)
 postCheckoutFile=${gitPath}/.git/hooks/post-checkout
-FLAVOR=${FLAVOR:="cambodiaDebug"}
+FLAVOR=${FLAVOR:="ereferrals"}
 
 # Generate last commit
 sh ${gitPath}/generate_last_commit.sh
@@ -13,5 +13,5 @@ cd sdk
 git checkout 2.25_EyeSeeTea
 cd -
 
-mkdir app/src/${FLAVOR}/res/raw
-cp ${BUDDYBUILD_SECURE_FILES}/config.json app/src/${FLAVOR}/res/raw/config.json
+mkdir app/src/"${BUDDYBUILD_VARIANTS}${}"/res/raw
+cp ${BUDDYBUILD_SECURE_FILES}/config_${FLAVOR}.json app/src/${FLAVOR}/res/raw/config.json

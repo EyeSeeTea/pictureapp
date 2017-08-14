031672ccab1bb2aa9caf8b1788d31e1650cbcca6
idelcano <ignaciodelcano@gmail.com>
2017-08-14 21:22:23 +0200

    Create downloadermediastrategy in all the variants

---
+++ app/src/laos/java/org/eyeseetea/malariacare/data/remote/strategies/DownloaderMediaStrategy.java
@@ -0,0 +1,10 @@
+package org.eyeseetea.malariacare.data.remote.strategies;
+
+import com.google.api.services.drive.Drive;
+import org.eyeseetea.malariacare.data.repositories.MediaRepository;
+
+public class DownloaderMediaStrategy {
+
+    public static void init(Drive mService, MediaRepository mMediaRepository,  String rootUid){
+    }
+}

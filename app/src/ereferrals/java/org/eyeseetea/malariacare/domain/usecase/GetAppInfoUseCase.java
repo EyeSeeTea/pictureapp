c968ffa71c6e9fd29b238edab2484b90f7d7a347
Jorge Sánchez <xurxosf@gmail.com>
2017-08-28 09:33:43 +0200

    execute usecase in async mode and notify loaded data in main thread

--- app/src/ereferrals/java/org/eyeseetea/malariacare/domain/usecase/GetAppInfoUseCase.java
+++ app/src/ereferrals/java/org/eyeseetea/malariacare/domain/usecase/GetAppInfoUseCase.java
@@ -1,5 +1,6 @@
 package org.eyeseetea.malariacare.domain.usecase;
 
+import org.eyeseetea.malariacare.domain.boundary.executors.IAsyncExecutor;
 import org.eyeseetea.malariacare.domain.boundary.executors.IMainExecutor;
 import org.eyeseetea.malariacare.domain.boundary.repositories.IAppInfoRepository;
 import org.eyeseetea.malariacare.domain.entity.AppInfo;
@@ -7,28 +8,42 @@ import org.eyeseetea.malariacare.domain.entity.AppInfo;
 public class GetAppInfoUseCase implements UseCase {
 
     private IMainExecutor mMainExecutor;
+    private IAsyncExecutor mAsyncExecutor;
     private IAppInfoRepository mAppInfoRepository;
     private Callback mCallback;
 
     public GetAppInfoUseCase(
             IMainExecutor mainExecutor,
+            IAsyncExecutor asyncExecutor,
             IAppInfoRepository appInfoRepository) {
         mMainExecutor = mainExecutor;
+        mAsyncExecutor = asyncExecutor;
         mAppInfoRepository = appInfoRepository;
     }
 
     public void execute(Callback callback) {
         mCallback = callback;
-        mMainExecutor.run(this);
+        mAsyncExecutor.run(this);
     }
 
     @Override
     public void run() {
-        mCallback.onGetAppInfo(mAppInfoRepository.getAppInfo());
+        final AppInfo appInfo = mAppInfoRepository.getAppInfo();
+
+        notifyAppInfo(appInfo);
+    }
+
+    private void notifyAppInfo(final AppInfo appInfo) {
+        mMainExecutor.run(new Runnable() {
+            @Override
+            public void run() {
+                mCallback.onAppInfoLoaded(appInfo);
+            }
+        });
     }
 
 
     public interface Callback {
-        void onGetAppInfo(AppInfo appInfo);
+        void onAppInfoLoaded(AppInfo appInfo);
     }
 }

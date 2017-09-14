0e0e882cba350b03755750a34102adabe7ab1554
Idelcano <ignaciodelcano@gmail.com>
2017-09-14 17:58:16 +0200

    remove obsolete tests

--- app/src/test/java/org/eyeseetea/malariacare/domain/entity/DeviceTest.java
+++ app/src/test/java/org/eyeseetea/malariacare/domain/entity/DeviceTest.java
@@ -7,21 +7,7 @@ import org.junit.rules.ExpectedException;
 public class DeviceTest {
 
     @Rule
-    public ExpectedException thrown = ExpectedException.none();
-
-    @Test
-    public void should_throw_exception_if_phone_is_empty() {
-        thrown.expect(IllegalArgumentException.class);
-
-        new Device("", "kfndmnfdmf", "v7");
-    }
-
-    @Test
-    public void should_throw_exception_if_imei_is_empty() {
-        thrown.expect(IllegalArgumentException.class);
-
-        new Device("6568333832", "", "v7");
-    }
+    public ExpectedException thrown = ExpectedException.none();  
 
     @Test
     public void should_throw_exception_if_androidversion_is_null() {

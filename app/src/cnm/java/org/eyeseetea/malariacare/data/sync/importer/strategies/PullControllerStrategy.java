af4a1cc22e8f4c7b28b24dbb8c2c3250c815de13
mplazaspalacio <manuelplazaspalacio@gmail.com>
2017-09-19 12:24:29 +0200

    preparing pull an credentials for autoconfigure feature

--- app/src/cnm/java/org/eyeseetea/malariacare/data/sync/importer/strategies/PullControllerStrategy.java
+++ app/src/cnm/java/org/eyeseetea/malariacare/data/sync/importer/strategies/PullControllerStrategy.java
@@ -1,12 +1,44 @@
 package org.eyeseetea.malariacare.data.sync.importer.strategies;
 
+import android.util.Log;
+
 import org.eyeseetea.malariacare.data.sync.importer.ConvertFromSDKVisitor;
 import org.eyeseetea.malariacare.data.sync.importer.PullController;
+import org.eyeseetea.malariacare.domain.boundary.IPullController;
+import org.eyeseetea.malariacare.domain.entity.OrganisationUnit;
+import org.eyeseetea.malariacare.domain.usecase.pull.PullFilters;
+import org.eyeseetea.malariacare.domain.usecase.pull.PullStep;
 
 public class PullControllerStrategy extends APullControllerStrategy {
     public PullControllerStrategy(PullController pullController) {
         super(pullController);
     }
+
+    @Override
+    public void pull(PullFilters pullFilters, IPullController.Callback callback) {
+        Log.d(TAG, "Starting PULL process...");
+        try {
+
+            callback.onStep(PullStep.METADATA);
+            mPullController.populateMetadataFromCsvs(pullFilters.isDemo());
+            if(pullFilters.isAutoConfig()) {
+                OrganisationUnit organisationUnit = getOrgUnitByPhone();
+            }
+            if (organisationUnit != null|| pullFilters.isDemo()) {
+                callback.onComplete();
+            } else {
+                mPullController.pullMetada(pullFilters, callback);
+            }
+        } catch (Exception ex) {
+            Log.e(TAG, "pull: " + ex.getLocalizedMessage());
+            callback.onError(ex);
+        }
+    }
+
+    private OrganisationUnit getOrgUnitByPhone() {
+        return null;
+    }
+
     @Override
     public void convertMetadata(ConvertFromSDKVisitor converter) {
 

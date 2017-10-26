812198f2ac29cbc36396c259ba90e6b8e0755142
idelcano <ignaciodelcano@gmail.com>
2017-10-26 14:39:51 +0200

    move login to background

--- app/src/cnm/java/org/eyeseetea/malariacare/domain/usecase/LoginUseCase.java
+++ app/src/cnm/java/org/eyeseetea/malariacare/domain/usecase/LoginUseCase.java
@@ -1,6 +1,8 @@
 package org.eyeseetea.malariacare.domain.usecase;
 
 import org.eyeseetea.malariacare.domain.boundary.IAuthenticationManager;
+import org.eyeseetea.malariacare.domain.boundary.executors.IAsyncExecutor;
+import org.eyeseetea.malariacare.domain.boundary.executors.IMainExecutor;
 import org.eyeseetea.malariacare.domain.entity.Credentials;
 import org.eyeseetea.malariacare.domain.entity.UserAccount;
 import org.eyeseetea.malariacare.domain.exception.ConfigJsonIOException;
@@ -11,36 +13,25 @@ import java.net.MalformedURLException;
 import java.net.UnknownHostException;
 
 
-public class LoginUseCase extends ALoginUseCase {
+public class LoginUseCase extends ALoginUseCase implements UseCase {
     private IAuthenticationManager mAuthenticationManager;
+    private IAsyncExecutor mAsyncExecutor;
+    private IMainExecutor mMainExecutor;
+    Credentials mCredentials;
+    Callback mCallback;
 
-    public LoginUseCase(IAuthenticationManager authenticationManager) {
+    public LoginUseCase(IAuthenticationManager authenticationManager, IAsyncExecutor asyncExecutor,
+            IMainExecutor mainExecutor) {
+        mMainExecutor = mainExecutor;
+        mAsyncExecutor = asyncExecutor;
         mAuthenticationManager = authenticationManager;
     }
 
     @Override
     public void execute(final Credentials credentials, final Callback loginCallback) {
-        if (credentials == null) {
-            onErrorCallback(loginCallback,
-                    new IllegalArgumentException("Credentials could not be null"));
-        } else {
-            mAuthenticationManager.login(credentials,
-                    new IAuthenticationManager.Callback<UserAccount>() {
-                        @Override
-                        public void onSuccess(UserAccount userAccount) {
-                            if (!credentials.isDemoCredentials()) {
-                                logoutAndHardcodedLogin(credentials, loginCallback);
-                            } else {
-                                loginCallback.onLoginSuccess();
-                            }
-                        }
-
-                        @Override
-                        public void onError(Throwable throwable) {
-                            onErrorCallback(loginCallback, throwable);
-                        }
-                    });
-        }
+        mCredentials = credentials;
+        mCallback = loginCallback;
+        mAsyncExecutor.run(this);
     }
 
     private void logoutAndHardcodedLogin(final Credentials credentials,
@@ -76,19 +67,88 @@ public class LoginUseCase extends ALoginUseCase {
     }
 
     private void onErrorCallback(final Callback callback, Throwable throwable) {
+        throwable.printStackTrace();
         if (throwable instanceof MalformedURLException
                 || throwable instanceof UnknownHostException) {
-            callback.onServerURLNotValid();
+            onServerURLNotValid();
         } else if (throwable instanceof InvalidCredentialsException) {
-            callback.onInvalidCredentials();
+            onInvalidCredentials();
         } else if (throwable instanceof NetworkException) {
-            callback.onNetworkError();
+            onNetworkError();
         } else if (throwable instanceof ConfigJsonIOException) {
-            callback.onConfigJsonInvalid();
+            onConfigJsonInvalid();
         } else {
-            throwable.printStackTrace();
-            callback.onUnexpectedError();
+            onUnexpectedError();
         }
 
+    }
+
+    private void onUnexpectedError() {
+        mMainExecutor.run(new Runnable() {
+            @Override
+            public void run() {
+                mCallback.onUnexpectedError();
+            }
+        });
+    }
+
+    private void onConfigJsonInvalid() {
+        mMainExecutor.run(new Runnable() {
+            @Override
+            public void run() {
+                mCallback.onConfigJsonInvalid();
+            }
+        });
+    }
+
+    private void onNetworkError() {
+        mMainExecutor.run(new Runnable() {
+            @Override
+            public void run() {
+                mCallback.onNetworkError();
+            }
+        });
+    }
+
+    private void onInvalidCredentials() {
+        mMainExecutor.run(new Runnable() {
+            @Override
+            public void run() {
+                mCallback.onInvalidCredentials();
+            }
+        });
+    }
+
+    private void onServerURLNotValid() {
+        mMainExecutor.run(new Runnable() {
+            @Override
+            public void run() {
+                mCallback.onServerURLNotValid();
+            }
+        });
+    }
+    @Override
+    public void run() {
+        if (mCredentials == null) {
+            onErrorCallback(mCallback,
+                    new IllegalArgumentException("Credentials could not be null"));
+        } else {
+            mAuthenticationManager.login(mCredentials,
+                    new IAuthenticationManager.Callback<UserAccount>() {
+                        @Override
+                        public void onSuccess(UserAccount userAccount) {
+                            if (!mCredentials.isDemoCredentials()) {
+                                logoutAndHardcodedLogin(mCredentials, mCallback);
+                            } else {
+                                mCallback.onLoginSuccess();
+                            }
+                        }
+
+                        @Override
+                        public void onError(Throwable throwable) {
+                            onErrorCallback(mCallback, throwable);
+                        }
+                    });
+        }
     }
 }

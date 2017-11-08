3676674a6c04eeedbb2a717232c75a3ddf30a5cd
ifoche <nacho.foche@gmail.com>
2017-11-08 20:33:49 +0100

    add missing formats & remove unnecessary code

--- app/src/ereferrals/java/utils/PhoneMask.java
+++ app/src/ereferrals/java/utils/PhoneMask.java
@@ -2,30 +2,7 @@ package utils;
 
 import org.eyeseetea.malariacare.domain.entity.PhoneFormat;
 
-/**
- * Created by ina on 02/08/2016.
- */
 public class PhoneMask {
-
-    /**
-     * Plain telephone mask: number stars with 00|+ and have 11 more digits or starts with
-     * 07|06|+|+2556|+2557 and have 9 more digits
-     */
-    public static final String PLAIN_PHONE_NUMBER_MASK = "";
-
-    public static String formatPhoneNumber(String phoneValue) {
-        //Empty -> nothing to format
-        if (phoneValue == null) {
-            phoneValue = "";
-        }
-
-        //Already formatted -> done
-        if (phoneValue.isEmpty() || phoneValue.matches(PLAIN_PHONE_NUMBER_MASK)) {
-            return phoneValue;
-        }
-
-        return phoneValue;
-    }
 
     public static boolean checkPhoneNumberByMask(String phoneValue, PhoneFormat phoneFormat) {
 

937f63b104a55aa46beb3676ee0bb859b73085c6
mplazaspalacio <manuelplazaspalacio@gmail.com>
2018-02-12 22:56:54 +0100

    Solving merge bug.

--- app/src/main/java/org/eyeseetea/malariacare/domain/entity/Question.java
+++ app/src/main/java/org/eyeseetea/malariacare/domain/entity/Question.java
@@ -1,6 +1,6 @@
 package org.eyeseetea.malariacare.domain.entity;
 
-import static org.eyeseetea.malariacare.domain.utils.RequiredChecker.required;<<<<<<< HEAD
+import static org.eyeseetea.malariacare.domain.utils.RequiredChecker.required;
 
 import java.util.List;
 

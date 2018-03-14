a1d63c3f39e41a1e97aadfcbc56ccec3fd0181f2
arturomejiamarmol <arturomejiamarmol@gmail.com>
2018-03-14 00:38:41 -0400

    Migration 14: Dropping and recreating DB Schema

---
+++ app/src/cnm/java/org/eyeseetea/malariacare/data/database/migrations/Migration14DropAllTables.java
@@ -0,0 +1,32 @@
+package org.eyeseetea.malariacare.data.database.migrations;
+
+
+import com.raizlabs.android.dbflow.annotation.Migration;
+import com.raizlabs.android.dbflow.config.FlowManager;
+import com.raizlabs.android.dbflow.sql.migration.BaseMigration;
+import com.raizlabs.android.dbflow.structure.Model;
+import com.raizlabs.android.dbflow.structure.ModelAdapter;
+import com.raizlabs.android.dbflow.structure.database.DatabaseWrapper;
+
+import org.eyeseetea.malariacare.data.database.AppDatabase;
+import org.eyeseetea.malariacare.data.database.utils.populatedb.PopulateDB;
+
+@Migration(version = 14, database = AppDatabase.class)
+public class Migration14DropAllTables extends BaseMigration {
+
+    @Override
+    public void migrate(DatabaseWrapper database) {
+
+        dropAndRecreateDBSchema(database);
+
+    }
+
+    private void dropAndRecreateDBSchema(DatabaseWrapper database) {
+        for (Class<? extends Model> s : PopulateDB.allTables) {
+            ModelAdapter modelAdapter = FlowManager.getModelAdapter(s);
+            String tableName = modelAdapter.getTableName();
+            database.execSQL("DROP TABLE IF EXISTS " + tableName);
+            database.execSQL(modelAdapter.getCreationQuery());
+        }
+    }
+}

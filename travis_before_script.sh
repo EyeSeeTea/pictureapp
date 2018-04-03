f03e352de0278e55c99a60392cc0b6e9f6afe809
idelcano <ignaciodelcano@gmail.com>
2018-04-03 09:52:21 +0200

    unzip encoded files

--- travis_before_script.sh
+++ travis_before_script.sh
@@ -2,4 +2,5 @@
 echo "start travis before script"
 openssl aes-256-cbc -K $encrypted_7829ffbf9381_key -iv $encrypted_7829ffbf9381_iv -in app.tar.enc -out app.tar -d
 echo "decrypted file"
+tar -xvzf app.tar
 echo "continue"

41ff1bfb285947b928d05835c9002e38703ecc2b
Idelcano <ignaciodelcano@gmail.com>
2018-05-07 19:44:03 +0200

    set as no data the download reference if the pull data is disabled

--- app/src/main/java/org/eyeseetea/malariacare/LoginActivity.java
+++ app/src/main/java/org/eyeseetea/malariacare/LoginActivity.java
@@ -154,11 +154,6 @@ public class LoginActivity extends Activity {
 
         //add spinner
         Spinner spinner = (Spinner) findViewById(R.id.data_spinner);
-        if(BuildConfig.pullDataDropdown) {
-            spinner.setVisibility(View.VISIBLE);
-        }else{
-            (findViewById(R.id.date_spinner_container)).setVisibility(View.GONE);
-        }
         spinner.setAdapter(spinnerArrayAdapter);
 
         spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
@@ -176,6 +171,12 @@ public class LoginActivity extends Activity {
             spinner.setSelection(spinnerArrayAdapter.getPosition(getString(R.string.no_data)));
         } else {
             spinner.setSelection(spinnerArrayAdapter.getPosition(dateLimit));
+        }
+        if(BuildConfig.pullDataDropdown) {
+            spinner.setVisibility(View.VISIBLE);
+        }else{
+            (findViewById(R.id.date_spinner_container)).setVisibility(View.GONE);
+            PreferencesState.getInstance().setDataLimitedByDate(getString(R.string.no_data));
         }
     }
 

5dce594ad3efb4cf8d4234154420119a120816c2
Idelcano <ignaciodelcano@gmail.com>
2018-05-10 01:11:27 +0200

    validate phone on init

--- app/src/main/java/org/eyeseetea/malariacare/views/question/multiquestion/PhoneMultiQuestionView.java
+++ app/src/main/java/org/eyeseetea/malariacare/views/question/multiquestion/PhoneMultiQuestionView.java
@@ -5,15 +5,23 @@ import android.widget.EditText;
 
 import org.eyeseetea.malariacare.BuildConfig;
 import org.eyeseetea.malariacare.R;
+import org.eyeseetea.malariacare.data.database.datasources.PhoneFormatLocalDataSource;
 import org.eyeseetea.malariacare.data.database.model.ValueDB;
+import org.eyeseetea.malariacare.domain.boundary.executors.IAsyncExecutor;
+import org.eyeseetea.malariacare.domain.boundary.executors.IMainExecutor;
+import org.eyeseetea.malariacare.domain.boundary.repositories.IPhoneFormatRepository;
+import org.eyeseetea.malariacare.domain.entity.Phone;
+import org.eyeseetea.malariacare.domain.entity.PhoneFormat;
 import org.eyeseetea.malariacare.domain.entity.Validation;
+import org.eyeseetea.malariacare.domain.exception.InvalidPhoneException;
+import org.eyeseetea.malariacare.domain.usecase.GetPhoneFormatUseCase;
+import org.eyeseetea.malariacare.presentation.executors.AsyncExecutor;
+import org.eyeseetea.malariacare.presentation.executors.UIThreadExecutor;
 import org.eyeseetea.malariacare.views.question.AKeyboardQuestionView;
 import org.eyeseetea.malariacare.views.question.IMultiQuestionView;
 import org.eyeseetea.malariacare.views.question.IQuestionView;
-import org.eyeseetea.malariacare.views.question.multiquestion.strategies
-        .APhoneMultiquestionViewStrategy;
-import org.eyeseetea.malariacare.views.question.multiquestion.strategies
-        .PhoneMultiquestionViewStrategy;
+import org.eyeseetea.malariacare.views.question.multiquestion.strategies.APhoneMultiquestionViewStrategy;
+import org.eyeseetea.malariacare.views.question.multiquestion.strategies.PhoneMultiquestionViewStrategy;
 import org.eyeseetea.sdk.presentation.views.CustomEditText;
 import org.eyeseetea.sdk.presentation.views.CustomTextView;
 
@@ -50,10 +58,34 @@ public class PhoneMultiQuestionView extends AKeyboardQuestionView implements IQu
             mCustomEditText.setText(valueDB.getValue());
             if (BuildConfig.validationInline) {
                 if (!mCustomEditText.getText().toString().isEmpty()) {
-                    Validation.getInstance().removeInputError(mCustomEditText);
+                        validatePhone(valueDB.getValue());
                 }
             }
         }
+    }
+
+    private void validatePhone(final String value) {
+
+        IPhoneFormatRepository phoneLocalDataSource = new PhoneFormatLocalDataSource();
+        IMainExecutor mainExecutor = new UIThreadExecutor();
+        IAsyncExecutor asyncExecutor = new AsyncExecutor();
+        GetPhoneFormatUseCase getPhoneFormatUseCase = new GetPhoneFormatUseCase(
+                phoneLocalDataSource, mainExecutor, asyncExecutor);
+        getPhoneFormatUseCase.execute(new GetPhoneFormatUseCase.Callback() {
+            @Override
+            public void onSuccess(final PhoneFormat phoneFormat) {
+                        try{
+                            new Phone(value, phoneFormat);
+                            Validation.getInstance().removeInputError(mCustomEditText);
+                        } catch (InvalidPhoneException e) {
+                        }
+                    }
+
+            @Override
+            public void onError() {
+
+            }
+        });
     }
 
     @Override

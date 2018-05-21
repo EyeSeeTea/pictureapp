61bf56b94de5668e8a82d6dd806c7906f884fe81
idelcano <ignaciodelcano@gmail.com>
2018-05-21 13:14:43 +0200

    move divisor to dropdownStrategy

--- app/src/ereferrals/java/org/eyeseetea/malariacare/strategies/DropdownAdapterStrategy.java
+++ app/src/ereferrals/java/org/eyeseetea/malariacare/strategies/DropdownAdapterStrategy.java
@@ -1,7 +1,7 @@
 package org.eyeseetea.malariacare.strategies;
 
 import android.content.Context;
-import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.support.v4.content.ContextCompat;
 import android.view.View;
 import android.widget.ListView;
@@ -16,8 +16,17 @@ public class DropdownAdapterStrategy {
         int selected = ContextCompat.getColor(context, android.R.color.secondary_text_dark);
 
         if((parent!=null && parent instanceof ListView && ((ListView)parent).getAdapter()!=null)) {
-            if(((ListView) parent).getCheckedItemPosition()==position){
-                convertView.setBackgroundColor(selected);
+            if(((ListView) parent) instanceof  ListView){
+                ListView listView = ((ListView)parent);
+                if(listView.getCheckedItemPosition()==position){
+                    convertView.setBackgroundColor(selected);
+                }
+                ColorDrawable myColor = new ColorDrawable(
+                        PreferencesState.getInstance().getContext().getResources().getColor(
+                                R.color.black_grey_purple)
+                );
+                listView.setDivider(myColor);
+                listView.setDividerHeight(1);
             }
             }else{
                 convertView.setBackgroundColor(transparent);

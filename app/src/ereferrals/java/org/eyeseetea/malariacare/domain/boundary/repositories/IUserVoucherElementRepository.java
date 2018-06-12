7dbbfb06103d2b9adb3938378e2e6164c1550882
idelcano <ignaciodelcano@gmail.com>
2018-06-12 11:26:46 +0200

    remove code

--- app/src/ereferrals/java/org/eyeseetea/malariacare/domain/boundary/repositories/IUserVoucherElementRepository.java
+++
@@ -1,8 +0,0 @@
-package org.eyeseetea.malariacare.domain.boundary.repositories;
-
-import org.eyeseetea.malariacare.domain.entity.intent.UserVoucher;
-
-public interface IUserVoucherElementRepository {
-    void sendUserVoucher(UserVoucher userVoucher);
-    void createUserVoucher(UserVoucher userVoucher);
-}

c51291ab0e7f60f2ad211ae4bc1ac4adffb2c44e
idelcano <ignaciodelcano@gmail.com>
2018-07-17 00:47:35 +0200

    remove commented code and unused imports

--- app/src/main/java/org/eyeseetea/malariacare/views/question/multiquestion/DropdownWithFilterMultiQuestionView.java
+++ app/src/main/java/org/eyeseetea/malariacare/views/question/multiquestion/DropdownWithFilterMultiQuestionView.java
@@ -1,13 +1,11 @@
 package org.eyeseetea.malariacare.views.question.multiquestion;
 
 import android.content.Context;
-import android.graphics.Color;
 import android.support.v7.app.AlertDialog;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.View;
 import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
@@ -19,8 +17,6 @@ import org.eyeseetea.malariacare.data.database.model.QuestionDB;
 import org.eyeseetea.malariacare.data.database.model.ValueDB;
 import org.eyeseetea.malariacare.domain.entity.Validation;
 import org.eyeseetea.malariacare.layout.adapters.general.DialogSpinnerAdapter;
-import org.eyeseetea.malariacare.layout.adapters.general.OptionArrayAdapter;
-import org.eyeseetea.malariacare.strategies.DropdownAdapterStrategy;
 import org.eyeseetea.malariacare.views.question.AOptionQuestionView;
 import org.eyeseetea.malariacare.views.question.IMultiQuestionView;
 import org.eyeseetea.malariacare.views.question.IQuestionView;
@@ -182,91 +178,9 @@ public class DropdownWithFilterMultiQuestionView extends AOptionQuestionView imp
 
     }
 
-   //@Override
-   //public void setImage(String path) {
-   //}
-
-   //@Override
-   //public void setValue(ValueDB valueDB) {
-       //if (valueDB == null || valueDB.getValue() == null) {
-       //    return;
-       //}
-       //optionSetFromSavedValue = true;
-
-       //for (int i = 0; i < listView.getAdapter().getCount(); i++) {
-       //    OptionDB optionDB = (OptionDB) listView.getItemAtPosition(i);
-       //    if (optionDB.equals(valueDB.getOptionDB())) {
-       //        listView.setSelection(i);
-       //        break;
-       //    }
-       //}
-
-       //if (BuildConfig.validationInline) {
-       //    if (listView.getSelectedItemPosition() > 0) {
-       //        Validation.getInstance().removeInputError(header);
-       //        header.setError(null);
-       //    } else {
-       //        Validation.getInstance().addinvalidInput(header, getContext().getString(
-       //                R.string.error_empty_question));
-       //    }
-       //}
- //   }
-
     @Override
     public boolean hasError() {
         return false;
-    }
-
-    private void init2(final Context context) {
-      //  inflate(context, R.layout.multi_question_tab_dropdown_row, this);
-//
-      //  header = (CustomTextView) findViewById(R.id.row_header_text);
-      //  spinnerOptions = (Spinner) findViewById(R.id.answer);
-      //  imageView = ((ImageView) findViewById(R.id.question_image_row));
-      //  optionSetFromSavedValue = true;
-//
-      //  spinnerOptions.setFocusable(true);
-      //  spinnerOptions.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-      //      @Override
-      //      public void onItemSelected(AdapterView<?> parent, View v, int position, long id) {
-      //          OptionDB optionDB = (OptionDB) parent.getItemAtPosition(position);
-      //          if (!optionSetFromSavedValue) {
-      //              if (position > 0) {
-      //                  notifyAnswerChanged(optionDB);
-      //              }
-      //          } else {
-      //              optionSetFromSavedValue = false;
-      //          }
-      //          if (BuildConfig.validationInline) {
-      //              if (position > 0) {
-      //                  Validation.getInstance().removeInputError(header);
-      //                  header.setError(null);
-      //              } else {
-      //                  Validation.getInstance().addinvalidInput(header, getContext().getString(
-      //                          R.string.error_empty_question));
-      //              }
-      //          }
-      //      }
-//
-      //      @Override
-      //      public void onNothingSelected(AdapterView<?> parent) {
-//
-      //      }
-      //  });
-      //  spinnerOptions.setOnTouchListener(new OnTouchListener() {
-      //      @Override
-      //      public boolean onTouch(View v, MotionEvent event) {
-      //          if (event.getAction() == MotionEvent.ACTION_UP) {
-      //              hideKeyboard(v);
-      //          }
-      //          return false;
-      //      }
-      //  });
-      //  if (BuildConfig.validationInline) {
-      //      Validation.getInstance().addInput(header);
-      //      Validation.getInstance().addinvalidInput(header,
-      //              getResources().getString(R.string.error_empty_question));
-      //  }
     }
 
     @Override

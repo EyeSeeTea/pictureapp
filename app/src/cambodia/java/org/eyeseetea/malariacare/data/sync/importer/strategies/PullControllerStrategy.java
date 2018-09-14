779526859304b1e2cccc167bef1e856898caac13
Idelcano <ignaciodelcano@gmail.com>
2018-09-14 20:30:01 +0200

    Add some missing cambodia imports

--- app/src/cambodia/java/org/eyeseetea/malariacare/data/sync/importer/strategies/PullControllerStrategy.java
+++ app/src/cambodia/java/org/eyeseetea/malariacare/data/sync/importer/strategies/PullControllerStrategy.java
@@ -2,6 +2,7 @@ package org.eyeseetea.malariacare.data.sync.importer.strategies;
 
 import org.eyeseetea.malariacare.data.sync.importer.ConvertFromSDKVisitor;
 import org.eyeseetea.malariacare.data.sync.importer.PullController;
+import org.eyeseetea.malariacare.domain.boundary.IPullController;
 
 public class PullControllerStrategy extends APullControllerStrategy {
     public PullControllerStrategy(PullController pullController) {

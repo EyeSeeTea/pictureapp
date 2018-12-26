aaf9809277ce2af0fc8a47cd8fe6e3fe62655a2a
mplazaspalacio <manuelplazaspalacio@gmail.com>
2018-12-26 13:48:04 +0100

    Getting if api available when login and when making push

---
+++ app/src/ereferrals/java/org/eyeseetea/malariacare/data/sync/exporter/model/ApiAvailable.java
@@ -0,0 +1,27 @@
+package org.eyeseetea.malariacare.data.sync.exporter.model;
+
+public class ApiAvailable {
+    private boolean available;
+    private  String msg;
+
+    public ApiAvailable(boolean available, String msg) {
+        this.available = available;
+        this.msg = msg;
+    }
+
+    public boolean isAvailable() {
+        return available;
+    }
+
+    public void setAvailable(boolean available) {
+        this.available = available;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+}

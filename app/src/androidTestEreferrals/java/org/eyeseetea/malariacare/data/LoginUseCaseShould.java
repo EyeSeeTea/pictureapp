f0c96f11489abe99aeb447d655d9d41c96af90ad
idelcano <ignaciodelcano@gmail.com>
2019-01-08 11:24:01 +0100

    refactor test

--- app/src/androidTestEreferrals/java/org/eyeseetea/malariacare/data/LoginUseCaseShould.java
+++ app/src/androidTestEreferrals/java/org/eyeseetea/malariacare/data/LoginUseCaseShould.java
@@ -1,70 +1,71 @@
 package org.eyeseetea.malariacare.data;
 
-import android.content.Context;
-import android.content.SharedPreferences;
-import android.preference.PreferenceManager;
-import android.support.test.InstrumentationRegistry;
-import android.support.test.rule.ActivityTestRule;
-
 import org.eyeseetea.malariacare.AssetsFileReader;
-import org.eyeseetea.malariacare.CommonTestPreferencesControl;
-import org.eyeseetea.malariacare.LoginActivity;
-import org.eyeseetea.malariacare.R;
+import org.eyeseetea.malariacare.data.authentication.AuthenticationManager;
+import org.eyeseetea.malariacare.data.remote.AuthenticationWSDataSource;
+import org.eyeseetea.malariacare.data.remote.IForgotPasswordDataSource;
 import org.eyeseetea.malariacare.data.server.CustomMockServer;
+import org.eyeseetea.malariacare.data.sync.exporter.eReferralsAPIClient;
+import org.eyeseetea.malariacare.domain.boundary.IConnectivityManager;
+import org.eyeseetea.malariacare.domain.boundary.executors.IAsyncExecutor;
+import org.eyeseetea.malariacare.domain.boundary.executors.IMainExecutor;
+import org.eyeseetea.malariacare.domain.boundary.repositories.ICredentialsRepository;
+import org.eyeseetea.malariacare.domain.boundary.repositories.IInvalidLoginAttemptsRepository;
+import org.eyeseetea.malariacare.domain.boundary.repositories.ISettingsRepository;
 import org.eyeseetea.malariacare.domain.entity.Credentials;
+import org.eyeseetea.malariacare.domain.entity.InvalidLoginAttempts;
 import org.eyeseetea.malariacare.domain.usecase.LoginUseCase;
-import org.eyeseetea.malariacare.factories.AuthenticationFactoryStrategy;
+import org.eyeseetea.malariacare.presentation.executors.UIThreadExecutor;
+import org.junit.Assert;
 import org.junit.Before;
-import org.junit.Rule;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
 
 import java.io.IOException;
 
 import static org.junit.Assert.fail;
+import static org.mockito.Mockito.when;
 
-public class LoginUseCaseShould extends CommonTestPreferencesControl {
+@RunWith(MockitoJUnitRunner.class)
+public class LoginUseCaseShould {
 
     private static final String AUTH_OK = "auth_ok.json";
     private static final String API_AVAILABLE_OK = "api_available_ok.json";
     private static final String API_AVAILABLE_NO_OK = "api_available_no_ok.json";
+    Credentials credentials;
     private CustomMockServer mCustomMockServer;
-    private LoginUseCase mLoginUseCase;
-    private int countSync;
-
-    @Rule
-    public ActivityTestRule<LoginActivity> mActivityRule = new ActivityTestRule<>(
-            LoginActivity.class);
+    @Mock
+    IConnectivityManager connectivityManager;
+    @Mock
+    IAuthenticationDataSource authenticationLocalDataSource;
+    @Mock
+    IForgotPasswordDataSource forgotPasswordDataSource;
+    @Mock
+    ISettingsRepository settingsRepository;
+    @Mock
+    ICredentialsRepository mCredentialsRepository;
+    @Mock
+    IInvalidLoginAttemptsRepository mInvalidLoginAttemptsRepository;
 
     @Before
     public void cleanUp() throws IOException {
         mCustomMockServer = new CustomMockServer(new AssetsFileReader());
-        savePreviousPreferences();
-        saveTestCredentialsAndProgram();
-        Context context = InstrumentationRegistry.getTargetContext();
-        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(
-                context);
-        SharedPreferences.Editor editor = sharedPreferences.edit();
-        editor.putString(context.getString(R.string.web_service_url),
-                mCustomMockServer.getBaseEndpoint());
-        editor.commit();
-        mLoginUseCase = new AuthenticationFactoryStrategy()
-                .getLoginUseCase(mActivityRule.getActivity());
+        credentials = new Credentials(mCustomMockServer.getBaseEndpoint(), "test", "test");
     }
 
     @Test
-    public void call_on_login_success_callback_when_server_is_available_during_login() throws IOException, InterruptedException {
-        final Object syncObject = new Object();
+    public void call_on_login_success_callback_when_server_is_available_during_login() throws IOException {
+
+        LoginUseCase mLoginUseCase = givenALoginUseCase();
         mCustomMockServer.enqueueMockResponseFileName(200, API_AVAILABLE_OK);
         mCustomMockServer.enqueueMockResponseFileName(200, AUTH_OK);
-        mLoginUseCase.execute(new Credentials(mCustomMockServer.getBaseEndpoint(), "test", "test"), new LoginUseCase.Callback() {
+
+        mLoginUseCase.execute(credentials, new LoginUseCase.Callback() {
             @Override
             public void onLoginSuccess() {
-                synchronized (syncObject) {
-                    if (countSync == 0) {
-                        syncObject.notify();
-                    }
-                    countSync++;
-                }
+                Assert.assertTrue(true);
             }
 
             @Override
@@ -108,16 +109,14 @@ public class LoginUseCaseShould extends CommonTestPreferencesControl {
             }
 
         });
-
-        synchronized (syncObject) {
-            syncObject.wait();
-        }
     }
 
     @Test
-    public void call_on_server_not_available_callback_when_server_is_not_available_during_login() throws IOException, InterruptedException {
-        final Object syncObject = new Object();
+    public void call_on_server_not_available_callback_when_server_is_not_available_during_login() throws IOException {
+
+        LoginUseCase mLoginUseCase = givenALoginUseCase();
         mCustomMockServer.enqueueMockResponseFileName(200, API_AVAILABLE_NO_OK);
+
         mLoginUseCase.execute(new Credentials(mCustomMockServer.getBaseEndpoint(), "test", "test"), new LoginUseCase.Callback() {
             @Override
             public void onLoginSuccess() {
@@ -161,18 +160,29 @@ public class LoginUseCaseShould extends CommonTestPreferencesControl {
 
             @Override
             public void onServerNotAvailable() {
-                synchronized (syncObject) {
-                    if (countSync == 0) {
-                        syncObject.notify();
-                    }
-                    countSync++;
-                }
+                Assert.assertTrue(true);
             }
 
         });
+    }
 
-        synchronized (syncObject) {
-            syncObject.wait();
-        }
+    private LoginUseCase givenALoginUseCase() {
+        when(connectivityManager.isDeviceOnline()).thenReturn(true);
+        IAuthenticationDataSource authenticationDataSource = new AuthenticationWSDataSource(new eReferralsAPIClient(mCustomMockServer.getBaseEndpoint()));
+
+        AuthenticationManager authenticationManager = new AuthenticationManager(authenticationLocalDataSource, authenticationDataSource, forgotPasswordDataSource, settingsRepository);
+
+        IMainExecutor mainExecutor = new UIThreadExecutor();
+        IAsyncExecutor asyncExecutor = new IAsyncExecutor() {
+            @Override
+            public void run(Runnable runnable) {
+                runnable.run();
+            }
+        };
+
+        when(mCredentialsRepository.getLastValidCredentials()).thenReturn(credentials);
+
+        when(mInvalidLoginAttemptsRepository.getInvalidLoginAttempts()).thenReturn(new InvalidLoginAttempts(0,30));
+        return new LoginUseCase(connectivityManager, authenticationManager, mainExecutor, asyncExecutor, mCredentialsRepository, mInvalidLoginAttemptsRepository);
     }
 }
